b27a6c6e6c2fca847aa4d6110aa30d1ac34696c8
rival104 <navid.rvl@gmail.com>
2018-02-25 11:23:20 -0500

    Create coming-soon-page

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function(){
+
+	ts = new Date(2018, 1, 25, 8)
+
+	// if((new Date() > ts)){
+	// 	ts = (new Date()).getTime() + 10*24*60*60*1000;
+
+	// }
+
+	/* ---- Countdown timer ---- */
+	$('#counter').countdown({
+		
+		// timestamp : (new Date()).getTime() + 24*10*60*60*1000
+		timestamp: ts.getTime() + 10 * 24 * 60 * 60 * 1000
+	});
+
+
+	/* ---- Animations ---- */
+
+	$('#links a').hover(
+		function(){ $(this).animate({ left: 3 }, 'fast'); },
+		function(){ $(this).animate({ left: 0 }, 'fast'); }
+	);
+
+	$('footer a').hover(
+		function(){ $(this).animate({ top: 3 }, 'fast'); },
+		function(){ $(this).animate({ top: 0 }, 'fast'); }
+	);
+
+
+
+
+});
